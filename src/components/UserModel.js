acf55fd3c7910c05ad4346cb0190a06560a3327d
KoJaem <woalswhwh@naver.com>
2022-06-21 12:25:11 +0900

    feat : GLTFLoader 추가

--- src/components/UserModel.js
+++ src/components/UserModel.js
@@ -4,6 +4,8 @@ import testBoxObj from "assets/testBox.obj";
 import testBoxMtl from "assets/testBox.mtl";
 import { useFrame, useLoader } from "@react-three/fiber";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import seatGltf from 'assets/seat.gltf';
 
 export const UserModel = () => {
   const materials = useLoader(MTLLoader, testBoxMtl);
@@ -17,12 +19,13 @@ export const UserModel = () => {
   useFrame(() => {
     modelRef.current.rotation.y += 0.01;
   });
-  
+    const gltf = useLoader(GLTFLoader, seatGltf);
 
 
   return (
     <mesh position={[0, 0, 0]} ref={modelRef}>
       <primitive object={obj} scale={1} />
+      <primitive object={gltf.scene} scale={0.1} />
     </mesh>
   );
 };
